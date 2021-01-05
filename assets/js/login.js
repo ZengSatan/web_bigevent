b1c09201e323e6e1d381ddf52f546a024ba5c837
ZengSatan <3262674113@qq.com>
2021-01-05 19:00:06 +0800

    完成了主页功能的开发

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,44 +1,41 @@
 $(function() {
-  // 点击“去注册账号”这个链接
-  $('#link_reg').on('click', function () {
+  // 点击“去注册账号”的链接
+  $('#link_reg').on('click', function() {
     $('.login-box').hide()
     $('.reg-box').show()
   })
 
-  // 点击“去登录”这个链接
-  $('#link_login').on('click', function () {
-    $('.login-box').show() 
+  // 点击“去登录”的链接
+  $('#link_login').on('click', function() {
+    $('.login-box').show()
     $('.reg-box').hide()
   })
 
-  // 从layui获取form对象，
-  var form = layui.form;
-  // 通过form.verify()函数自定义效验规则
+  // 从 layui 中获取 form 对象
+  var form = layui.form
+  var layer = layui.layer
+  // 通过 form.verify() 函数自定义校验规则
   form.verify({
-    // 自定义了一个叫pwd的效验规则
-    pwd: [
-      /^[\S]{6,12}$/
-      ,'密码必须6到12位，且不能出现空格'
-    ],
-    // 效验两次密码是否一致的规则
-    repwd: function (value) {
-      // 通过形参value拿到的是确认密码框的值
-      // 还需要拿到密码框的值
-      // 然后进行判断
-      // 如果判断失败，则return一个提示消息
-      let pwd = $('.reg-box [name=password]').val();
+    // 自定义了一个叫做 pwd 校验规则
+    pwd: [/^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'],
+    // 校验两次密码是否一致的规则
+    repwd: function(value) {
+      // 通过形参拿到的是确认密码框中的内容
+      // 还需要拿到密码框中的内容
+      // 然后进行一次等于的判断
+      // 如果判断失败,则return一个提示消息即可
+      var pwd = $('.reg-box [name=password]').val()
       if (pwd !== value) {
         return '两次密码不一致！'
       }
     }
   })
 
-  var layer = layui.layer;
   // 监听注册表单的提交事件
   $('#form_reg').on('submit', function(e) {
-    // 阻止默认的提交行为
+    // 1. 阻止默认的提交行为
     e.preventDefault()
-    // 发起Ajax的POST请求
+    // 2. 发起Ajax的POST请求
     var data = {
       username: $('#form_reg [name=username]').val(),
       password: $('#form_reg [name=password]').val()
